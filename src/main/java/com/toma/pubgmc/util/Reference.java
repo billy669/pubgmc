6180ace0a6a4856369e29571523e6d33eedb9576
Toma1O6 <42510241+Toma1O6@users.noreply.github.com>
2018-08-22 16:03:27 +0200

    Updated version

--- src/main/java/com/toma/pubgmc/util/Reference.java
+++ src/main/java/com/toma/pubgmc/util/Reference.java
@@ -4,7 +4,7 @@ public class Reference {
 
 	public static final String MOD_ID = "pubgmc";
 	public static final String NAME = "PUBGMC";
-	public static final String VERSION = "2.0";
+	public static final String VERSION = "2.1-a";
 	public static final String ACCEPTED_VERSIONS = "[1.12.2]";
 	public static final String CLIENT_PROXY_CLASS = "com.toma.pubgmc.proxy.ClientProxy";
 	public static final String COMMON_PROXY_CLASS = "com.toma.pubgmc.proxy.CommonProxy";
